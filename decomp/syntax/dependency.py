2beb48cdfa65d695426c14ecd8b7e930b1e735f2
aswhite <aaron.white@rochester.edu>
2019-06-02 11:10:37 -0400

    Completes lint of syntax module
    Former-commit-id: 7917230e14a68e6e5b5613897911f0615414291e

--- decomp/syntax/dependency.py
+++ decomp/syntax/dependency.py
@@ -1,6 +1,10 @@
+# pylint: disable=R1717
+# pylint: disable=R0903
+"""Module for building/containing dependency trees from CoNLL"""
+
 from numpy import array
-from ..corpus import Corpus
 from networkx import DiGraph
+from ..corpus import Corpus
 
 CONLL_HEAD = {'u': ['id', 'form', 'lemma', 'upos', 'xpos',
                     'feats', 'head', 'deprel', 'deps', 'misc'],
@@ -10,7 +14,8 @@ CONLL_HEAD = {'u': ['id', 'form', 'lemma', 'upos', 'xpos',
 CONLL_NODE_ATTRS = {'u': {k: CONLL_HEAD['u'].index(k)
                           for k in ['form', 'lemma', 'upos', 'xpos', 'feats']},
                     'x': {k: CONLL_HEAD['x'].index(k)
-                          for k in ['form', 'lemma', 'cpostag', 'postag', 'feats']}}
+                          for k in ['form', 'lemma', 'cpostag',
+                                    'postag', 'feats']}}
 
 CONLL_EDGE_ATTRS = {'u': {k: CONLL_HEAD['u'].index(k)
                           for k in ['deprel']},
@@ -19,23 +24,28 @@ CONLL_EDGE_ATTRS = {'u': {k: CONLL_HEAD['u'].index(k)
 
 
 class CoNLLDependencyTreeCorpus(Corpus):
-    """Class for building and containing dependency trees from CoNLL-U annotated sentences
+    """Class for building/containing dependency trees from CoNLL-U
 
     Attributes
     ----------
-    trees : list(Tree)
+    graphs : list(networkx.DiGraph)
         trees constructed from annotated sentences
+    graphids : list(str)
+        ids for trees constructed from annotated sentences
+    ngraphs : int
+        number of graphs in corpus
     """
 
     def _graphbuilder(self, graphid, rawgraph):
         return DependencyGraphBuilder.from_conll(rawgraph, graphid)
-    
-class DependencyGraphBuilder(object):
-    """a dependency graph builder"""
-    
+
+
+class DependencyGraphBuilder:
+    """A dependency graph builder"""
+
     @classmethod
     def from_conll(cls, conll, treeid='', spec='u'):
-        """build DiGraph from a CoNLL representation
+        """Build DiGraph from a CoNLL representation
 
         Parameters
         ----------
@@ -43,17 +53,17 @@ class DependencyGraphBuilder(object):
             conll representation
         treeid : str
             a unique identifier for the tree
-        spec  : str (default="u")
+        spec  : {"u", "x"} (default="u")
             the specification to assume of the conll representation
         """
 
         # handle null treeids
         treeid = treeid+'-' if treeid else ''
-        
+
         # initialize the dependency graph
         depgraph = DiGraph(conll=array(conll))
         depgraph.name = treeid.strip('-')
-        
+
         depgraph.add_nodes_from([cls._conll_node_attrs(treeid, row, spec)
                                  for row in conll])
         depgraph.add_edges_from([cls._conll_edge_attrs(treeid, row, spec)
@@ -64,10 +74,10 @@ class DependencyGraphBuilder(object):
     @staticmethod
     def _conll_node_attrs(treeid, row, spec):
         node_id = row[0]
-        
+
         node_attrs = {}
         other_attrs = {}
-        
+
         for attr, idx in CONLL_NODE_ATTRS[spec].items():
             # convert features into a dictionary
             if attr == 'feats':
@@ -80,14 +90,14 @@ class DependencyGraphBuilder(object):
                 node_attrs[attr] = row[idx]
 
         node_attrs = dict(node_attrs, **other_attrs)
-                
+
         return (treeid+'syntax-'+node_id, node_attrs)
 
     @staticmethod
     def _conll_edge_attrs(treeid, row, spec):
         child_id = treeid+'syntax-'+row[0]
         parent_id = treeid+'syntax-'+row[CONLL_HEAD[spec].index('head')]
-        edge_attrs = {attr: row[idx] for attr, idx in CONLL_EDGE_ATTRS[spec].items()}
-        
+        edge_attrs = {attr: row[idx]
+                      for attr, idx in CONLL_EDGE_ATTRS[spec].items()}
+
         return (parent_id, child_id, edge_attrs)
-    
