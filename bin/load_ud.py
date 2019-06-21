f7679defd2c7dfb6e7458dd868c340b0af4fafa6
aswhite <aaron.white@rochester.edu>
2019-06-21 11:29:14 -0400

    Updates class method for loading predpatt corpus
    Former-commit-id: bb98fcc4dee180b79cb2b52f43884dedf8e4477c
    Former-commit-id: 7061b08d9efc496e8f858e46071d0d4c1b34d7c2

--- bin/load_ud.py
+++ bin/load_ud.py
@@ -1,6 +1,6 @@
 from os.path import basename
 from decomp.semantics.predpatt import PredPattCorpus
 
-predpatt = {fname: PredPattCorpus.from_file('../data/UD_English-r1.2/'+fname,
+predpatt = {fname: PredPattCorpus.from_conll('../data/UD_English-r1.2/'+fname,
                                             name='ewt-'+basename(fname).split('.')[0].split('-')[2])
             for fname in ['en-ud-train.conllu','en-ud-dev.conllu','en-ud-test.conllu']}
